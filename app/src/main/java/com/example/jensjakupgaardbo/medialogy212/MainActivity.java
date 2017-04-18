c06c7d444b6fc07b0706a686096e0f334acc0fb1
Simone Danielsen <sibeda@h35.aau1x.klient.sydhavn.site.aau.dk>
2017-04-18 15:11:20 +0200

    Added button that opens the video page

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/MainActivity.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jensjakupgaardbo.medialogy212;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void nextPage(View view) {
+        Intent intent = new Intent(getApplicationContext(), ActivityVideoPage.class);
+
+        startActivity(intent);
+    }
+
+
+ }
+
+
+
+
+
