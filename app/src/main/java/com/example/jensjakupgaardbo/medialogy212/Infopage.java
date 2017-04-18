90f2f919b767a5680908300dccf5dccf0f9758c2
Hax3r <dgkartin@live.dk>
2017-04-18 11:07:02 +0200

    Try to weird Bottom bug

---
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/Infopage.java
@@ -0,0 +1,17 @@
+package com.example.jensjakupgaardbo.medialogy212;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.paolorotolo.appintro.*;
+
+public class Infopage extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
