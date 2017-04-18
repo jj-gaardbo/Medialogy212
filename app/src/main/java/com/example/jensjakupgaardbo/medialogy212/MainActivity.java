f4756341406b27f23efa1368b635e5a1336a9af2
CakeFTW <isager-kruuse@hotmail.com>
2017-04-18 11:58:15 +0200

    added alarm activity, and button to get to it

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/MainActivity.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jensjakupgaardbo.medialogy212;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoAlarm(View view) {
+        Intent openAlarmPage = new Intent(getApplicationContext(), AlarmActivity.class);
+        //pack stuff with it
+        startActivity(openAlarmPage);
+
+
+
     }
 }
