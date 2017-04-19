18b55d0884f5fedd5e13721fabbe8fa0b2b3d4ed
CakeFTW <isager-kruuse@hotmail.com>
2017-04-19 17:26:36 +0200

    Database now stores a Latitude and longtitude for every alarm, updated constructors and addAlarm methods accordingly

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/Alarm.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/Alarm.java
@@ -1,19 +1,22 @@
 package com.example.jensjakupgaardbo.medialogy212;
 
 // class for use in the database of the app, this is the base class that tells us what to save about the alarms
-
+import com.google.android.gms.maps.model.LatLng;
 public class Alarm {
 
     //these are the attributes we want to save in each alarm
     private int _id;
     private String _alarmname;
+    private LatLng _latlng;
+
 
     public Alarm() {
 
     }
 
-    public Alarm(String alarmname) {
+    public Alarm(String alarmname, LatLng location) {
         this._alarmname = alarmname;
+        this._latlng = location;
     }
 
 
@@ -33,5 +36,11 @@ public class Alarm {
         this._alarmname = _alarmname;
     }
 
+    public LatLng get_latlng() {
+        return _latlng;
+    }
 
+    public void set_latlng(LatLng _latlng) {
+        this._latlng = _latlng;
+    }
 }
