100b024152e978efa4f3e889eed78fb680a4a8d8
CakeFTW <isager-kruuse@hotmail.com>
2017-04-21 15:24:46 +0200

    Theres now an alarm reciever class, that can be used to set alarms

---
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/AlarmReceiver.java
@@ -0,0 +1,26 @@
+package com.example.jensjakupgaardbo.medialogy212;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+/**
+ * Created by Rasmus on 21-04-2017.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver{
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /*PackageManager pm = context.getPackageManager();
+        Intent launchIntent = pm.getLaunchIntentForPackage("C:\\Users\\Rasmus\\Documents\\GitHub\\Medialogy212\\app\\src\\main\\java\\com\\example\\jensjakupgaardbo\\medialogy212\\dataBaseOverview.java");
+        launchIntent.putExtra("some_data", "value");
+        context.startActivity(launchIntent);
+*/
+        Intent openDb = new Intent(context, dataBaseOverview.class);
+        context.startActivity(openDb);
+        //Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
+    }
+}
