713b41ace6e44896f16dc533a5a9f823bb10877c
CakeFTW <isager-kruuse@hotmail.com>
2017-04-26 13:03:52 +0200

    Working on updated design(partial commit)

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/CardsAdapter.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/CardsAdapter.java
@@ -1,7 +1,6 @@
 package com.example.jensjakupgaardbo.medialogy212;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,10 +27,23 @@ class CardsAdapter extends ArrayAdapter<Alarm> {
         Alarm singleAlarm = getItem(position);
         TextView alarmTextView = (TextView) customView.findViewById(R.id.singleCardTextView);
         ImageView cardImage = (ImageView) customView.findViewById(R.id.singleCardImageView);
+        TextView nearMarkerTextView = (TextView) customView.findViewById(R.id.nearMarkerTextVIew);
+        TextView activeMarkerTextView = (TextView) customView.findViewById(R.id.markerActiveTextView);
+        TextView wakeUptime = (TextView) customView.findViewById(R.id.wakeUpTextView);
+
+        Switch activeSwitch = (Switch) customView.findViewById(R.id.activeSwitch) ;
 
 
         alarmTextView.setText((CharSequence) singleAlarm.get_alarmname());
         cardImage.setImageResource(R.drawable.card);
+        nearMarkerTextView.setText("Near Marker");
+        if(singleAlarm.isActive()){
+            activeMarkerTextView.setText("Active");
+            activeSwitch.toggle();
+        }else{
+            activeMarkerTextView.setText("deactive");
+        }
+        wakeUptime.setText(singleAlarm.getWakeTimeOfDay(2));
 
         return customView;
     }
