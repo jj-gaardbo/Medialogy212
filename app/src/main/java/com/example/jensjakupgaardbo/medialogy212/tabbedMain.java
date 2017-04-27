140d4102ddc5a3fd4ac2e34240ef652f378bdeac
Hax3r <dgkartin@live.dk>
2017-04-27 16:36:04 +0200

    Made alarmPage use icons instead of ugly buttons

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/tabbedMain.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/tabbedMain.java
@@ -159,7 +159,7 @@ public class tabbedMain extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
+            // Show 2 total pages.
             return 2;
         }
 
