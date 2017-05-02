d946f111f1d24c0a286d89e8950037e39b0fa873
CakeFTW <isager-kruuse@hotmail.com>
2017-05-02 09:47:37 +0200

    more math fixes

--- app/src/main/java/com/example/jensjakupgaardbo/medialogy212/tabbedMain.java
+++ app/src/main/java/com/example/jensjakupgaardbo/medialogy212/tabbedMain.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -27,8 +26,6 @@ import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
 
 public class tabbedMain extends AppCompatActivity {
 
